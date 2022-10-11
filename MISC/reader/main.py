17dfed6cf9b82870695609ba5c0daf0b555476c9
grandff <dsaqaz421@naver.com>
2022-10-11 21:25:10 +0800

    Feat: reader 2

--- MISC/reader/main.py
+++ MISC/reader/main.py
@@ -1,14 +1,14 @@
 import sys
 
-def send(data, end='\n'):
-    sys.stdout.write(data + end)
+def send(data, end='\n'):   # end는 개행문자로
+    sys.stdout.write(data + end)    # 개행문자가 포함이 안되어있음. 그래서 끝마다 end를 붙여놨음
     sys.stdout.flush()
 
 def read():
-    return raw_input()
+    return raw_input()  # 파이썬 2버전에서만 사용하는 기능임
 
 def filtering(filename):
-    filter = ['flag', 'proc', 'self', 'etc', 'tmp', 'home', '~', '.', '*', '?', '\\', 'x']
+    filter = ['flag', 'proc', 'self', 'etc', 'tmp', 'home', '~', '.', '*', '?', '\\', 'x'] # 이 키워드가 들어가면 필터링 처리
     for i in filter:
         if i in filename:
             send("Filtered!")
